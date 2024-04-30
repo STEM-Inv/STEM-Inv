f10a7a45e4aaf69c48433ec314f26404d24b949c
lmm077 <1398159954@qq.com>
2024-04-30 15:19:34 +0800

    Update run_editing.sh

--- TokenFlow-Edit/run_editing.sh
+++ TokenFlow-Edit/run_editing.sh
@@ -3,10 +3,10 @@
 export GIT_PYTHON_REFRESH=quiet
 export HUGGINGFACE_TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxxx
 
-CUDA_VISIBLE_DEVICES=5 python preprocess.py \
+python preprocess.py \
  --config_path ./configs/config_pnp_mallard.yaml
 
-CUDA_VISIBLE_DEVICES=5 python run_tokenflow_pnp.py \
+python run_tokenflow_pnp.py \
     --config_path ./configs/config_pnp_mallard.yaml
 
 
